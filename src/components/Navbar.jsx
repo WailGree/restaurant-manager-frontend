48901972265dff08e1476d5e967dfe7283e1b6f7
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-03 16:37:25 +0200

    New approach to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,21 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-
-const useStyles = makeStyles({
-    root: {
-        flexGrow: 1,
-    },
-});
+import { Tabs, Tab, AppBar } from '@material-ui/core';
 
 function Navbar() {
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
+    const [selectedValue, setSelectedTab] = React.useState(0);
 
     const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-
+        setSelectedTab(newValue);
+    }
     return (
-        <Paper className={classes.root}>
-            <Tabs
-                value={value}
-                onChange={handleChange}
-                indicatorColor="primary"
-                textColor="primary"
-                centered
-            >
-                <Tab label="Home page" />
-                <Tab label="Menu" />
-                <Tab label="Contact" />
-            </Tabs>
-        </Paper>
+        <div>
+            <AppBar>
+                <Tabs value={selectedValue} onChange={handleChange} centered>
+                    <Tab label="Home page" />
+                    <Tab label="Contact" />
+                </Tabs>
+            </AppBar>
+        </div>
     )
 }
 
