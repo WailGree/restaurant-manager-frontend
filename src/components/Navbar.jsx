6003795d60f7dec0df618291fe146d4f2abb8d24
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-03 16:52:21 +0200

    Basic display of Homepage and Contact

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import { Tabs, Tab, AppBar } from '@material-ui/core';
+import Homepage from './Homepage';
+import Contact from './Contact';
 
 function Navbar() {
-    const [selectedValue, setSelectedTab] = React.useState(0);
+    const [selectedTab, setSelectedTab] = React.useState(0);
 
     const handleChange = (event, newValue) => {
         setSelectedTab(newValue);
     }
     return (
-        <div>
-            <AppBar>
-                <Tabs value={selectedValue} onChange={handleChange} centered>
-                    <Tab label="Home page" />
-                    <Tab label="Contact" />
-                </Tabs>
-            </AppBar>
-        </div>
+        <>
+            <div>
+                <AppBar position="static">
+                    <Tabs value={selectedTab} onChange={handleChange} centered>
+                        <Tab label="Home page" />
+                        <Tab label="Contact" />
+                    </Tabs>
+                </AppBar>
+                {selectedTab === 0 && <Homepage />}
+                {selectedTab === 1 && <Contact />}
+            </div>
+        </>
     )
 }
 
