624c5491c4cf02056596ba34a9fe87e13df6217d
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-03 16:51:50 +0200

    Move router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Homepage from './components/Homepage';
 
 function App() {
+
   return (
     <div className="App">
       <Navbar />
+      
     </div>
   );
 }
