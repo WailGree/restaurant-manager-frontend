ec1580ad9b9990d979f1d589613664e68b505cbd
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-03 14:41:27 +0200

    Make Material-UI navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+    },
+});
 
 function Navbar() {
-    return (
-        <div>
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
 
-        </div>
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Paper className={classes.root}>
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                centered
+            >
+                <Tab label="Home page" />
+                <Tab label="Menu" />
+                <Tab label="Contact" />
+            </Tabs>
+        </Paper>
     )
 }
 
