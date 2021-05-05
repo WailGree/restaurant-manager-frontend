393ba2e5f2a72cc043727d3f98930f1f1771f5e0
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-05 20:58:27 +0200

    Set size of MenuItem

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,44 @@
+import MenuItem from './MenuItem';
+import { Grid, makeStyles } from '@material-ui/core';
+
 function Menu() {
+    const gridItemxl = 2;
+    const gridItemlg = 3;
+    const gridItemmd = 4;
+    const gridItemsm = 5;
+    const gridItemxs = 12;
+
+    const useStyle = makeStyles({
+        containerGrid: {
+            paddingLeft: '20px',
+            paddingRight: '20px'
+        }
+    })
+
+    const classes = useStyle();
 
     return (
         <div>
-
+            <Grid container spacing={4} alignContent="center" alignItems="center" className={classes.gridContainer}>
+                <Grid item xs={gridItemxs} xl={gridItemxl} lg={gridItemlg} sm={gridItemsm} md={gridItemmd}>
+                    <MenuItem />
+                </Grid>
+                <Grid item xs={gridItemxs} xl={gridItemxl} lg={gridItemlg} sm={gridItemsm} md={gridItemmd}>
+                    <MenuItem />
+                </Grid>
+                <Grid item xs={gridItemxs} xl={gridItemxl} lg={gridItemlg} sm={gridItemsm} md={gridItemmd}>
+                    <MenuItem />
+                </Grid>
+                <Grid item xs={gridItemxs} xl={gridItemxl} lg={gridItemlg} sm={gridItemsm} md={gridItemmd}>
+                    <MenuItem />
+                </Grid>
+                <Grid item xs={gridItemxs} xl={gridItemxl} lg={gridItemlg} sm={gridItemsm} md={gridItemmd}>
+                    <MenuItem />
+                </Grid>
+                <Grid item xs={gridItemxs} xl={gridItemxl} lg={gridItemlg} sm={gridItemsm} md={gridItemmd}>
+                    <MenuItem />
+                </Grid>
+            </Grid>
         </div>
     )
 }
