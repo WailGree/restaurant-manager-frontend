9233c1bd2e189ef30de4615db460fc8b94fd8e7c
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-05 23:20:11 +0200

    Unfinished generalization

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,33 +2,6 @@ import { Card, Paper, Grid, CardMedia, CardContent, Typography } from "@material
 import { makeStyles } from '@material-ui/core/styles';
 
 export default function About() {
-    const useStyles = makeStyles((theme) => ({
-        root: {
-            flexGrow: 1
-        },
-        CardMedia: {
-            height: 0,
-            paddingTop: '56%'
-        },
-        paper: {
-            height: '80%',
-            width: '80%'
-        },
-        control: {
-            padding: theme.spacing(2)
-        }
-    }));
-
-    const classes = useStyles();
-
-    const authors = [{
-        0: {
-            name: "Roland Dominik Szilágyi"
-        },
-        1: {
-            name: "Máté Szathmári"
-        }
-    }]
 
     const gridItemxl = 2;
     const gridItemlg = 4;
@@ -36,26 +9,54 @@ export default function About() {
     const gridItemsm = 8;
     const gridItemxs = 12;
 
-    return (
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            flexGrow: 1,
+            padding: '5%' //Fixes page overflow
+        },
+        containerGrid: {
+            paddingLeft: '10%',
+            paddingRight: '10%',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+        },
+        card: {
+            width: 400,
+        },
+        cardMedia: {
+            height: 600,
+            width: 'auto',
+            paddingTop: '55%'
+        }
+    }));
+    const classes = useStyles();
 
+    const authors = [
+        {
+            name: 'Roland Dominik Szilágyi',
+            image: 'roland_dominik_szilagyi.png',
+
+        },
+        {
+            name: 'Máté Szathmári',
+            image: 'mate_szathmari.png'
+        }
+    ];
+
+    const gridItems = authors.map((index, author) => (
+        <Grid key={index} item xl={gridItemxl}>
+            <Card className={classes.card}>
+                <CardMedia
+                    className={classes.cardMedia}
+                    image={process.env.PUBLIC_URL + '/' + author.image} title={author.name + ' image'} />
+                <Typography>{author.name}</Typography>
+            </Card>
+        </Grid>
+    ));
+    return (
         <div>
-            <Grid container alignItems="flex-start" spacing={25}>
-                <Grid item xl={gridItemxl}>
-                    <Card>
-                        <CardMedia
-                            style={{ height: 600, paddingTop: '55%', leftPadding: '20px' }}
-                            image={process.env.PUBLIC_URL + '/roland_dominik_szilagyi.png'} title="Máté Szathmári" />
-                        <Typography>Roland Dominik Szilágyi</Typography>
-                    </Card>
-                </Grid>
-                <Grid item xl={gridItemxl}>
-                    <Card>
-                        <CardMedia
-                            style={{ height: 600, paddingTop: '55%' }}
-                            image={process.env.PUBLIC_URL + '/mate_szathmari.png'} title="Máté Szathmári" />
-                        <Typography>Máté Szathmári</Typography>
-                    </Card>
-                </Grid>
+            <Grid container spacing={0} className={classes.containerGrid}>
+                {gridItems}
             </Grid>
         </div>
     )
