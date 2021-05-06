09cc015bcc81b3bcac867b4d6c2eb01fa8f05179
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-06 12:46:55 +0200

    Delete unused imports

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -4,14 +4,6 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import MenuItemModal from './MenuItemModal';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 
 const useStyles = makeStyles({
   root: {
