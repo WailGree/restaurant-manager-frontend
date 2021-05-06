9f505db2c72d5188a5ffe0c82b1ad9fdc4a08cba
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-06 12:18:10 +0200

    Create MenuItemModal component

---
+++ src/components/MenuItemModal.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default function FormDialog({ item }) {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">{item.name}</DialogTitle>
+                <DialogContent>
+                    Image goes here
+          <DialogContentText>
+                        {item.description}
+                    </DialogContentText>
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
+}
