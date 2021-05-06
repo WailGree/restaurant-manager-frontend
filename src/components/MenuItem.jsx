6a0c00141e71dc4d77a0b5f46ba4baee4966bc13
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-06 11:11:30 +0200

    Display frame for menu items

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
@@ -24,31 +23,25 @@ const useStyles = makeStyles({
   },
 });
 
-export default function OutlinedCard() {
+export default function MenuItem(props) {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <Card className={classes.root} variant="outlined">
+      <CardActionArea centered>
+        Image comes here
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+
+          </Typography>
+          <Typography variant="h5" component="h2">
+            Item name{props.name}
+          </Typography>
+          <Typography variant="body2" component="p">
+            {props.description} description
         </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
